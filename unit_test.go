f3f0c365d08da2347c9fe7e4710c3934e4116072
Mohammed Massarwi <mohammedm@codevalue.net>
2020-01-07 10:31:49 +0200

    Adding Sum Test

--- unit_test.go
+++ unit_test.go
@@ -19,27 +19,8 @@ func TestUnit(t *testing.T) {
 
 		if body, err := ioutil.ReadAll(w.Body); err != nil {
 			t.Error(err)
-		} else if string(body) != x.reply { 
+		} else if string(body) != x.reply {
 			t.Error("oops we have a problem: expected reply - ", x.reply, ", but got - ", string(body))
-		}
-	}
-}
-func TestSum(t *testing.T) {
-	tables := []struct {
-		x int 
-		y int
-		n int
-	}{
-		{1, 1, 2},
-		{1, 2, 3},
-		{2, 2, 4},
-		{5, 2, 7},
-	}
-
-	for _, table := range tables {
-		total := Sum(table.x, table.y)
-		if total != table.n {
-			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
 		}
 	}
 }
